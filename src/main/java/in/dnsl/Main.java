8a1b90db5a6b07f60d92a3f37ec8e47a0037a4af
dnslin <i@dnsl.in>
2023-12-03 01:01:20 +0800

    feat: main测试类

---
+++ src/main/java/in/dnsl/Main.java
@@ -0,0 +1,11 @@
+package in.dnsl;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Main {
+    public static void main(String[] args) {
+        log.info("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
