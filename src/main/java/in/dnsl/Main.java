a756e5607458065760e06a7e801bb2cc0d3f3bb5
dnslin <i@dnsl.in>
2023-12-03 17:16:11 +0800

    test: 登录测试

--- src/main/java/in/dnsl/Main.java
+++ src/main/java/in/dnsl/Main.java
@@ -1,11 +1,11 @@
 package in.dnsl;
 
+import in.dnsl.logic.CloudLogin;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class Main {
     public static void main(String[] args) {
-        log.info("Hello, World!");
-        System.out.println("Hello, World!");
+        CloudLogin.login("xx", "xx.");
     }
 }
