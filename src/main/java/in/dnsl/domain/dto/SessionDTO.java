ea0254480958c6879a277724561560764d988bd1
dnslin <i@dnsl.in>
2023-12-03 12:07:30 +0800

    feat: 增加Session实体类

---
+++ src/main/java/in/dnsl/domain/dto/SessionDTO.java
@@ -0,0 +1,36 @@
+package in.dnsl.domain.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class SessionDTO {
+    @JsonProperty("res_code")
+    private Integer resCode;
+    @JsonProperty("res_message")
+    private String resMessage;
+    @JsonProperty("accessToken")
+    private String accessToken;
+    @JsonProperty("familySessionKey")
+    private String familySessionKey;
+    @JsonProperty("familySessionSecret")
+    private String familySessionSecret;
+    @JsonProperty("getFileDiffSpan")
+    private Integer getFileDiffSpan;
+    @JsonProperty("getUserInfoSpan")
+    private Integer getUserInfoSpan;
+    @JsonProperty("isSaveName")
+    private String isSaveName;
+    @JsonProperty("keepAlive")
+    private Integer keepAlive;
+    @JsonProperty("loginName")
+    private String loginName;
+    @JsonProperty("refreshToken")
+    private String refreshToken;
+    @JsonProperty("sessionKey")
+    private String sessionKey;
+    @JsonProperty("sessionSecret")
+    private String sessionSecret;
+}
